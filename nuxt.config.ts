c64b8fa9124d9e4733607641f67ec425c30daaa5
Crstlnz <52141479+crstlnz@users.noreply.github.com>
2024-01-26 06:28:34 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  ssr: false,
+  ssr: true,
   runtimeConfig: {
     public: {
       tenor_key: process.env.TENOR_API,
